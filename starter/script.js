4469a0e757addfe4de45321c5a8ec5287fcf4c16
KausVlad <kausvirtual@gmail.com>
2023-03-20 00:30:32 +0200

    Rest Pattern and Parameters

--- starter/script.js
+++ starter/script.js
@@ -40,8 +40,54 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
+// ⁡⁣⁣⁢1) REST Pattern Destructuring⁡
+// ⁡⁣⁢⁣SPREAD, because on RIGHT side of =⁡⁡⁡
+const arr = [1, 2, ...[3, 4]];
+
+// ⁡⁢⁣⁡⁣⁢⁣REST, because on LEFT side of =⁡⁡
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+
+console.log(sat, weekdays);
+
+// ⁡⁣⁣⁢2) Functions REST parameters
+
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 6, 7, 8);
+add(1, 2, 3, 4, 5, 6);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+/*
 // ⁡⁣⁣⁢SPREAD OPERATOR (...arr)⁡
 
 const arr = [7, 4, 1];
@@ -86,7 +132,7 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
-
+*/
 /*
 //⁡⁣⁣⁢Destructuring objects⁡
 restaurant.orderDelivery({
