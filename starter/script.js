54040e47373dcf57cbcd0c9bff91b680bffd9d66
KausVlad <kausvirtual@gmail.com>
2023-03-23 00:20:16 +0200

    ES6

--- starter/script.js
+++ starter/script.js
@@ -5,6 +5,24 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  // can also be written as [weekdays[5]]  or `day-${2 + 4}`, computed name
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 const restaurant = {
   name: 'Classico Italiano',
   locations: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,20 +30,9 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // ** openingHours: openingHours, // old way
+  openingHours, // new way, ES6 enhanced object literals
+
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -46,17 +53,20 @@ const restaurant = {
   },
 };
 
+/*
+//! For of loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const iterator of menu) console.log(iterator);
 
-//!! entries() important staff
+//!!  entries() important staff
 for (const [i, el] of menu.entries()) {
   console.log(`${i + 1}: ${el}`);
   // console.log(`${iterator[0] + 1}: ${iterator[1]}`);
 }
 
 console.log([...menu.entries()]);
+*/
 /*
 // ! ⁡⁣⁣⁢1) REST Pattern Destructuring⁡
 // ! ⁡⁣⁢⁣SPREAD, because on RIGHT side of =⁡⁡⁡
