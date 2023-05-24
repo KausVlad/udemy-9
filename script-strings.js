27d2bbb65b8008353a334f0fb949ae8c38c85d62
KausVlad <kausvirtual@gmail.com>
2023-05-24 20:42:36 +0300

    C H 4 in 9 part

--- script-strings.js
+++ script-strings.js
@@ -1,5 +1,53 @@
 'use strict';
 
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const textarea = document.querySelector('textarea');
+const button = document.querySelector('button');
+
+button.textContent = 'Click me!';
+
+button.addEventListener('click', () => {
+  const text = textarea.value;
+  for (const [key, i] of text.split('\n').entries()) {
+    const [fist, second] = i.toLowerCase().trim().split('_');
+    const newWord = fist + second.replace(second[0], second[0].toUpperCase());
+
+    console.log(`${newWord.padEnd(20)}${'💥'.repeat(key + 1)}`);
+  }
+});
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+/*
+
 // split and join
 
 console.log('a+veree+qwqe+qw'.split('+'));
@@ -58,7 +106,7 @@ const planesLine = (n) => {
 
 planesLine(5);
 planesLine(1);
-
+*/
 /*
 const airline = 'TAB Air Galaxy';
 const plane = 'A320';
